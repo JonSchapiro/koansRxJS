d0927df86ee6c87385c444b86f33a3c0565b7dae
Berkeley Martinez <Berkeley@robotie.com>
2014-06-27 18:46:32 -0700

    Am I?

--- koans/lesson1-observablestreams.js
+++ koans/lesson1-observablestreams.js
@@ -1,3 +1,4 @@
+"use strict";
 module('Lesson 1 - Observable Streams');
 
 /*
@@ -6,7 +7,7 @@ module('Lesson 1 - Observable Streams');
  * Step 3: run it again
  * Note: Do not change anything other than the blank
  */
- 
+
 test('ObjectsFirst', function () {
     var xs = L2O.Enumerable.fromArray([1, 2, 3]);
     equals(xs.first(), _______);
@@ -19,7 +20,7 @@ test('ObjectsFirst', function () {
 test('SimpleSubscription', function() {
     Rx.Observable
         .returnValue(42)
-        .subscribe(function(x) { equals(x, _______); });	
+        .subscribe(function(x) { equals(x, _______); })
 });
 
 test('SimpleReturn', function() {
@@ -45,7 +46,7 @@ test('EveryThingCounts', function() {
     Rx.Observable
         .fromArray(numbers)
         .subscribe(function(x) { received += x; });
-    equals(received, _______);	
+    equals(received, _______);
 });
 
 test('DoingInTheMiddle', function() {
@@ -57,7 +58,7 @@ test('DoingInTheMiddle', function() {
                 d + '=' + (d === 1 ? 'Study Like Mad' : _______));
             })
         .subscribe();
-	equals(
+  equals(
             status.toString(),
             '4=Party,3=Party,2=Party,1=Study Like Mad');
 });
@@ -69,8 +70,8 @@ test('NothingListensUntilYouSubscribe', function() {
         .doAction(function(n) { sum += n; });
 
     equals(0, sum);
-    
-    observable._______();	
 
-    equals(55, sum); 
+    observable._______();
+
+    equals(55, sum);
 });
